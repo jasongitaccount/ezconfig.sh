2eb86442cbe9aa577bd0b3856a59a6461c1661fe
jasongitaccount <69093295+jasongitaccount@users.noreply.github.com>
2020-08-27 23:41:49 -0400

    'add' operation added
    'add' operation was added. The handling of tab and space characters was improved.

--- ezconfig.sh
+++ ezconfig.sh
@@ -27,7 +27,7 @@
 # Usage example: ./thisscript.sh /var/file.conf set configkey1 = ON
 # The resulf of the example above would be setting "configkey1 = ON" in the file /var/file.conf
 
-version=0.1.5
+version=0.2.1
 
 firstargument=$1 # e.g. /var/file.conf
 operation=$2 # set|reset|autoset|autoreset
@@ -36,12 +36,12 @@ connector=$4 # e.g. =
 value=$5 # e.g. ON
 
 
-if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
+if [[ "${operation}" =~ ^('add'|'set'|'reset'|'autoset'|'autoreset')$ ]]; then
 	
 	filepath="${firstargument}"
 	
 	# Too few arguments or not separated (e.g. key=value).
-	if [ ! $connector ]; then
+	if [[ ! "${connector}" ]]; then
 		echo 'Error: too few arguments are provided! This may be because the key and value are not spaced out.'
 		echo 'Examples:'
 		echo 'Incorrect -> key=value'
@@ -51,23 +51,52 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 	fi
 
 	# Fix for the unexplicit space connector
-	if [ ! $value ]; then
-		value=$connector
+	if [[ ! "${value}" ]]; then
+		value="${connector}"
 		connector=' '
 	fi
+	
+	# Trimming the space and tab from the input for finding matches with different spacing patterns. The trimmed input will not be written into the file
+	key_trimmed=$(echo -e "${key}" | sed -r s$'\001''^[[:blank:]]*'$'\001'''$'\001' | sed -r s$'\001''[[:blank:]]*$'$'\001'''$'\001' )
+	connector_trimmed=$(echo -e "${connector}" | sed -r s$'\001''^[[:blank:]]*'$'\001'''$'\001' | sed -r s$'\001''[[:blank:]]*$'$'\001'''$'\001' )
+	value_trimmed=$(echo -e "${value}" | sed -r s$'\001''^[[:blank:]]*'$'\001'''$'\001' | sed -r s$'\001''[[:blank:]]*$'$'\001'''$'\001' )
+	
+	# Represent the space/tab connector that was removed during trimming. This is used only for match finding, not for replacing
+	if [[ "${connector_trimmed}" == '' ]]; then
+		# Check if there is any tab character in the connector
+		if [[ $(echo -e "${connector}" | grep -cP "\t") -gt 0 ]]; then
+			connector_trimmed=$(echo -e "\t")
+		else
+			connector_trimmed=' '
+		fi
+	fi
+	
+	# The default regex expression following the key (used in 'set','reset','autoset','autoreset')
+	_regex_afterkey='[^\.[:alnum:]_-]'
+	_regex_afterkey_trimmed='[^\.[:alnum:]_-]'
+	_regex_end='.*$'
 
+	# If this is an 'add' operation, the key, connector, and value should be included in the search
+	if [[ "${operation}" =~ ^('add')$ ]]; then
+		_regex_afterkey="[[:blank:]]*${connector}[[:blank:]]*${value}[[:blank:]]*\$"
+		_regex_afterkey_trimmed="[[:blank:]]*${connector_trimmed}[[:blank:]]*${value_trimmed}[[:blank:]]*\$"
+		_regex_end=''
+	fi
+	
 	# Colored grep matches for output
-	matches_pretty=$(grep -n --color=always "^[#[:space:]]*${key}[^\.[:alnum:]_-]" "${filepath}")
+	matches_pretty=$(grep -n --color=always "^[#[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}" "${filepath}")
 	# Grep matches without colors for processing
-	matches_raw=$(echo "${matches_pretty}" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g")
+	matches_raw=$(echo "${matches_pretty}" | sed -r s$'\001''\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]'$'\001'''$'\001'g )
 	# Number of matching lines
 	matches_count=$(echo -n "${matches_raw}" | grep -c '^')
 	# Matches that are not commented out
-	uncommented_matches=$(echo "${matches_raw}" | grep "^[[:digit:]]*\:[[:space:]]*${key}[^\.[:alnum:]_-]")
+	uncommented_matches=$(echo "${matches_raw}" | grep "^[[:digit:]]*\:[[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}")
 	# Number of matches that are not commented out
 	uncommented_matches_count=$(echo -n "${uncommented_matches}" | grep -c '^')
 	
-	# If there is any uncommented matches, try to update one of them instead of appending to the file
+	# If there is any uncommented matches...
+	# add: if it's an exact key/value match, do not add
+	# set,reset,autoset,autoreset: try to update one of them instead of appending to the file
 	if [[ "${uncommented_matches_count}" -gt 0 ]]; then
 		echo '> Matches before the processing:'
 		echo "${matches_pretty}"
@@ -75,7 +104,7 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 		# If there are multiple matches (at least one of them uncommented), update the last uncommented match
 		if [[ "${matches_count}" -gt 1 ]]; then
 			# If this is not an automated operation, prompt for confirmation
-			if [[ ! "${operation}" =~ ^(autoset|autoreset)$ ]]; then
+			if [[ ! "${operation}" =~ ^('autoset'|'autoreset')$ ]]; then
 				read -p "> There are ${matches_count} matches. I can modify only the last uncommented instance. Would you like to continue? (y/n) " -n 1 -r prompt_reply
 			else
 				prompt_reply='Y'
@@ -89,13 +118,13 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 			else
 				# Find the file line number of the last uncommented match and update it
 				linenum=$( echo "${uncommented_matches}" | tail -1 | cut -d: -f1 )
-				sed -i  "${linenum}"s$'\001'"^[[:space:]]*\(${key}\)[^\.[:alnum:]_-].*$"$'\001'"\1${connector}${value}"$'\001' "${filepath}"
+				sed -i "${linenum}"s$'\001'"^[[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}${_regex_end}"$'\001'"${key}${connector}${value}"$'\001' "${filepath}"
 			fi
 		# If there is only one match (must be uncommented), update it
 		else
 			# Find the file line number of the uncommented match and update it
 			linenum=$( echo "${uncommented_matches}" | cut -d: -f1 )
-			sed -i  "${linenum}"s$'\001'"^[[:space:]]*\(${key}\)[^\.[:alnum:]_-].*$"$'\001'"\1${connector}${value}"$'\001' "${filepath}"
+			sed -i "${linenum}"s$'\001'"^[[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}${_regex_end}"$'\001'"${key}${connector}${value}"$'\001' "${filepath}"
 		fi
 	# If there is no uncommented match present and the operation is "reset"/"autoreset", update the 
 	# last commented out line
@@ -125,7 +154,7 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 		else
 			# Find the file line number of the last uncommented match and update it
 			linenum=$( echo "${matches_raw}" | tail -1 | cut -d: -f1 )
-			sed -i  "${linenum}"s$'\001'"^[[:space:]]*#[[:space:]]*\(${key}\)[^\.[:alnum:]_-].*$"$'\001'"\1${connector}${value}"$'\001' "${filepath}"
+			sed -i "${linenum}"s$'\001'"^[[:blank:]]*#[[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}${_regex_end}"$'\001'"${key}${connector}${value}"$'\001' "${filepath}"
 		fi
 	# The operation is "set"/"autoset", so append to the file
 	else
@@ -134,7 +163,7 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 		# Check if there is an LF (\n) character at the end of the file
 		if [[ ! "${lasttwobytes}" =~ ('0a')$ ]]; then
 			# Check if there is an CRLF (\r\n) character in the file
-			if [[ $(grep -Uc $'\015' "${filepath}") -gt 0 ]]; then
+			if [[ $(grep -c $'\015' "${filepath}") -gt 0 ]]; then
 				echo -e "\r\n" >> "${filepath}"
 			else
 				echo -e "\n" >> "${filepath}"
@@ -145,7 +174,7 @@ if [[ "${operation}" =~ ^('set'|'reset'|'autoset'|'autoreset')$ ]]; then
 	fi
 	
 	echo "> Matches after the processing:"
-	grep --color=always -n "^[[:space:]]*${key}[^\.[:alnum:]_-]" "${filepath}"
+	grep --color=always -nP "^[#[:blank:]]*${key_trimmed}${_regex_afterkey_trimmed}" "${filepath}"
 	
 	cp /var/test.conf.bkup /var/test.conf
 
